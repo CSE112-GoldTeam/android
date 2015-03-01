be20fa2ab621dc4793785c0c9257ca9fca523772
msislam <mohammed.s.islam92@gmail.com>
2015-02-28 16:10:27 -0800

    Deleted the activites that are not needed any more, and implmented BusinessLoginActivity

---
+++ app/src/main/java/cse112goldteam/robobetty/BusinessLoginActivity.java
@@ -0,0 +1,34 @@
+package cse112goldteam.robobetty;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class BusinessLoginActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private EditText editTextEmail, editTextPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_login);
+
+        //TODO: add a check here to check if we already logged in. If so, then go to MainActivity
+
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonLogin){
+            String email = editTextEmail.getText().toString();
+            String password = editTextPassword.getText().toString();
+        }
+    }
+}
