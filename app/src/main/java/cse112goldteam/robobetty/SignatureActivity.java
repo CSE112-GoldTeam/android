930437319768801603aa477a310829ed59b4f605
msislam <mohammed.s.islam92@gmail.com>
2015-03-04 19:40:09 -0800

    fixed some bugzzzz

--- app/src/main/java/cse112goldteam/robobetty/SignatureActivity.java
+++ app/src/main/java/cse112goldteam/robobetty/SignatureActivity.java
@@ -1,20 +1,45 @@
 package cse112goldteam.robobetty;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 
 public class SignatureActivity extends ActionBarActivity implements View.OnClickListener{
+
+    private ImageView view;
+    private AnimationDrawable frameAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signature);
+
         this.findViewById(android.R.id.content).setBackgroundResource(Business.background);
         findViewById(R.id.buttonDoneSignActivity).setOnClickListener(this);
         findViewById(R.id.buttonPrev).setOnClickListener(this);
+        // Initialize ImageView and set animation on background
+        view = (ImageView) findViewById(R.id.imageAnimation);
+        view.setBackgroundResource(R.drawable.animation_list);
+
+        // Typecasting the Animation Drawable
+        frameAnimation = (AnimationDrawable) view.getBackground();
+    }
+
+    // Called when Activity becomes visible or invisible to the user
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Starting the animation when in Focus
+            frameAnimation.start();
+        } else {
+            // Stoping the animation when not in Focus
+            frameAnimation.stop();
+        }
     }
 
     @Override
